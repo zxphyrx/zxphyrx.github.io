02d655fee56ee7ebda49190fcc0a05ce197c9d96
Zeph Requilme <zephrequilme40@gmail.com>
2025-09-30 21:01:32 +0800

    Used constants for readability

--- src/canvas.js
+++ src/canvas.js
@@ -3,6 +3,13 @@ const ctx = canvas.getContext("2d");
 
 const scale = 0.5;
 
+const minSpeed = 0.3;
+const maxSpeed = 0.5;
+const minOpacity = 0.1;
+const maxOpacity = 0.2;
+const minAttractedOpacity = 0.8;
+const maxAttractedOpacity = 1;
+
 canvas.height = window.innerHeight * scale;
 canvas.width = window.innerWidth * scale;
 
@@ -44,9 +51,9 @@ function newBall() {
     let ball = {
         x: randomRange(0, canvas.width),
         y: randomRange(0, canvas.height),
-        speed: randomRange(0.3, 0.5) * scale,
+        speed: randomRange(minSpeed, maxSpeed) * scale,
         opacity: 0,
-        maxOpacityOrig: randomRange(0.1, 0.2),
+        maxOpacityOrig: randomRange(minOpacity, maxOpacity),
         maxOpacity: null,
         direction: randomRange(0, Math.PI * 2),
         size: 10 * scale,
@@ -97,7 +104,7 @@ function animate() {
             ball.direction = getAngle(ball, mousePos);
             if(!ball.attracted) {
                 ball.attracted = true;
-                ball.maxOpacity = randomRange(0.8, 1);
+                ball.maxOpacity = randomRange(minAttractedOpacity, maxAttractedOpacity);
             }
         } else {
             if(ball.attracted) {
@@ -106,9 +113,9 @@ function animate() {
                 ball.maxOpacity = ball.maxOpacityOrig;
             }
         }
-
+        
         if(ball.opacity < ball.maxOpacity) {
-            ball.opacity = Math.min(ball.opacity + 0.1, ball.maxOpacity);
+            ball.opacity = Math.min(ball.opacity + 0.075, ball.maxOpacity);
         }
         if(ball.opacity > ball.maxOpacity) {
             ball.opacity = Math.max(ball.opacity - 0.075, ball.maxOpacity);
@@ -119,7 +126,7 @@ function animate() {
         ball.x = newPos.x;
         ball.y = newPos.y;
         
-        if((ball.x - ball.size > canvas.width || ball.x - ball.size < 0 || ball.y - ball.size > canvas.height || ball.y - ball.size < 0 || ball.opacity < 0) && ball.dying == false) {
+        if((ball.x - ball.size > canvas.width || ball.x - ball.size < 0 || ball.y - ball.size > canvas.height || ball.y - ball.size < 0) && ball.dying == false) {
             balls.splice(index, 1);
             newBall();
         }
